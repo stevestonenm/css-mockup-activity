b146ec54970cafb02644ef9a01b5b7b23e1c672a
Rochelle Lewis <rlewis37@gmail.com>
2017-04-21 12:54:06 -0600

    checkpoint commit - add base files

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+
+		<!-- custom CSS stylesheet -->
+		<link rel="stylesheet" href="css/style.css" type="text/css" />
+
+		<title>CSS Mockup Activity</title>
+	</head>
+	<body>
+		Hai - main page
+	</body>
+</html>
---
+++ mockup/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+
+		<title>CSS Mockup Activity - Mockup</title>
+	</head>
+	<body>
+		Hai - mockup page
+	</body>
+</html>
