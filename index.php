22478a63be30d8d7125f71bd8b7e948e450e3aa9
Rochelle Lewis <rlewis37@gmail.com>
2017-04-23 01:20:17 -0600

    finalize HTML content, add CSS "head start"

--- index.php
+++ index.php
@@ -2,13 +2,95 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
 
 		<!-- custom CSS stylesheet -->
 		<link rel="stylesheet" href="css/style.css" type="text/css" />
 
 		<title>CSS Mockup Activity</title>
 	</head>
-	<body>
-		Hai - main page
+	<body class="sfooter">
+		<div class="sfooter-content">
+
+			<header>
+				<div class="container">
+					<h1>Dylan McDonald</h1>
+					<em>Computer Scientist. Cat Lover. Picard Fan.</em>
+				</div>
+			</header>
+
+			<main>
+
+				<!-- INSTRUCTIONS PANEL -->
+				<section>
+					<div class="container">
+						<div class="instructions">
+							<div class="pull-left">
+								<h2>Instructions</h2>
+								<ol>
+									<li>
+										Fork and clone the following GitHub repository, and create a new project and deployment.<br>
+										<a href="https://github.com/rlewis2892/css-mockup-activity" target="_blank">https://github.com/rlewis2892/css-mockup-activity</a>
+									</li>
+									<li>Edit the stylesheet, adding CSS to make the page look like the mockup. <a href="images/mockup.jpg" target="_blank">Mockup Image</a></li>
+									<li>Have fun! The solution will be demonstrated in class. <small>(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧</small></li>
+								</ol>
+							</div>
+							<div class="pull-right">
+								<img src="images/captain-janeway.png" alt="captain janeway" />
+							</div>
+							<div class="clearfix"></div>
+						</div><!--/.instructions-->
+					</div><!--/.container-->
+				</section>
+
+				<!-- SECTION ONE -->
+				<section>
+					<div class="container">
+						<img class="pull-left" src="images/stunning.jpg" alt="captain picard" />
+						<h2>A Day in the Life...</h2>
+						<p>Engage! Some days you get the bear, and some days the bear gets you. I'd like to think that I haven't changed those things, sir. Did you come here for something in particular or just general Riker-bashing? That might've been one of the shortest assignments in the history of Starfleet.</p>
+						<p>Hiss and stare at nothing then run suddenly away. Bare fangs at toy run hide in litter box until treats are fed toy mouse squeak roll over yet stare at the wall, play with food and get confused by dust. Chase dog then run away drink water out of the faucet for find a way to fit in tiny box. <span>Groom yourself 4 hours - checked, have your beauty sleep 18 hours - checked, be fabulous for the rest of the day - checked!</span></p>
+						<div class="clearfix"></div>
+					</div>
+				</section>
+
+				<!-- SECTION TWO -->
+				<section>
+					<div class="container">
+						<h2>Summary of Profe$$ional Skillz</h2>
+						<table>
+							<tr>
+								<th>Languages</th>
+								<th>Software</th>
+								<th>Other</th>
+							</tr>
+							<tr>
+								<td>Java</td>
+								<td>Linux Server</td>
+								<td>Certified Ethical Hacker</td>
+							</tr>
+							<tr>
+								<td>PHP</td>
+								<td>Metasploit</td>
+								<td>Star Trek Trivia Champion</td>
+							</tr>
+							<tr>
+								<td>Klingon</td>
+								<td>Microsoft Office</td>
+								<td>Meme Master</td>
+							</tr>
+						</table>
+					</div><!--/.container-->
+				</section>
+
+			</main>
+		</div><!--/.sfooter-content-->
+
+		<footer>
+			<div class="container">
+				<h4>&copy; 2017 A USS Berners-Lee Production</h4>
+			</div>
+		</footer>
 	</body>
 </html>
